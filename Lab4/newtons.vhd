2e6482c18ff8c0089d9071012514e9c7b5b2b197
Johnny Gaddis <johnnygaddis777@gmail.com>
2018-02-27 13:02:40 -0700

    y0 work

--- Lab4/newtons.vhd
+++ Lab4/newtons.vhd
@@ -4,8 +4,8 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity newtons is
-    port(newtons_input : in  std_logic_vector (35 downto 0);
-         y 	       : out std_logic_vector (35 downto 0));
+  port(y0 : in  std_logic_vector (35 downto 0);
+       y  : out std_logic_vector (35 downto 0));
 end entity;
 
 architecture rsr_arch of rsr is
--- Lab4/y0.vhd
+++ Lab4/y0.vhd
@@ -4,8 +4,8 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity y0 is
     port(clk   : in  std_logic;
-         x     : in  std_logic_vector (35 downto 0);
-         y0    : out std_logic_vector (35 downto 0));
+         x     : in  std_logic_vector (53 downto 0);
+         y0    : out std_logic_vector (53 downto 0));
 end entity;
 
 architecture y0_arch of y0 is
@@ -25,24 +25,29 @@ architecture y0_arch of y0 is
   signal zeros          : std_logic_vector (5 downto 0);
   signal addr, data_out : std_logic_vector (7 downto 0);
   signal B, A           : signed (13 downto 0);
-  signal Xa, Xb         : signed (35 downto 0);
+  signal Xa, Xb         : signed (53 downto 0);
 
   begin
 
-    A0: lzc port map (clk, x, zeros);
-    A1: rom port map (std_logic_vector(Xb(7 downto 0)), clk, data_out);
+    A0: lzc port map (clk, x (53 downto 18), zeros);
+    A1: rom port map (std_logic_vector(Xb(17 downto 10)), clk, data_out);
 
     process (clk)
       begin
         B(13 downto 8) <= 17 - signed(zeros);
-     	  if B(0) = '0' then
+        if B(0) = '0' then
           A <= shift_left(B, 1) - shift_right(B, 1);
         elsif B(0) = '1' then
-	        A <= "1000000" + shift_left(B, 1) - shift_right(B, 1);
+          A <= "1000000" + shift_left(B, 1) - shift_right(B, 1);
         end if;
         Xa <= shift_left(signed(x), to_integer(A));
         Xb <= shift_left(signed(x), to_integer(B));
-        Xb(35 downto 28) <= signed(data_out);
+        Xb(53 downto 46) <= signed(data_out);
+        if B(0) = '0' then
+          y0 <= std_logic_vector(signed(Xa) * signed(Xb));
+        elsif B(0) = '1' then
+          y0 <= std_logic_vector(signed(Xa) * signed(Xb) * "100000000000000000");
+        end if;
     end process;
 	
 end architecture;
