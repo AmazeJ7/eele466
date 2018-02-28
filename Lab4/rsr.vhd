16cca11bd604836e8eca557207920baabe9f4605
Johnny Gaddis <johnnygaddis777@gmail.com>
2018-02-28 10:35:43 -0700

    length changes

--- Lab4/rsr.vhd
+++ Lab4/rsr.vhd
@@ -5,24 +5,24 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity rsr is
   port(clk        : in  std_logic;
-       rsr_input  : in  std_logic_vector (53 downto 0);
-       rsr_output : out std_logic_vector (53 downto 0));
+       rsr_input  : in  std_logic_vector (35 downto 0);
+       rsr_output : out std_logic_vector (35 downto 0));
 end entity;
 
 architecture rsr_arch of rsr is
 
   component y0
     port(clk : in  std_logic;
-         x   : in  std_logic_vector(53 downto 0);
-         y0  : out std_logic_vector(53 downto 0));
+         x   : in  std_logic_vector(35 downto 0);
+         y0  : out std_logic_vector(35 downto 0));
   end component;
 
   component newtons
-    port(y0 : in  std_logic_vector(53 downto 0);
-         y  : out std_logic_vector(53 downto 0));
+    port(y0 : in  std_logic_vector(35 downto 0);
+         y  : out std_logic_vector(35 downto 0));
   end component;
 
-  signal y0out : std_logic_vector(53 downto 0);
+  signal y0out : std_logic_vector(35 downto 0);
 
   begin
 
